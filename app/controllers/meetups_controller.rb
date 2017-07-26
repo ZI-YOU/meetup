5282e3440363c00cbbc0476a973ee5d8738e0c0a
shiyongheng <ziyou@shiyonghengdeMacBook-Air.local>
2017-07-26 21:39:36 +0800

    Create meetup CRUD

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,48 @@
 class MeetupsController < ApplicationController
+  def index
+  	@meetups = Meetup.all
+  end
+
+  def show
+  	@meetup = Meetup.find(params[:id])
+  end
+
+  def new
+  	@meetup = Meetup.new(params[:id])
+  end
+
+  def create
+  	@meetup = Meetup.new(meetup_params)
+
+  	if @meetup.save
+  	  redirect_to root_path
+  	else
+  	  render :new
+  	end
+  end
+
+  def edit
+  	@meetup = Meetup.find(params[:id])
+  end
+
+  def update
+  	@meetup = Meetup.find(params[:id])
+  	if @meetup.update(meetup_params)
+  	  redirect_to root_path
+  	else
+  	  render :edit
+  	end
+  end
+
+  def destroy
+  	@meetup = Meetup.find(params[:id])
+  	@meetup.destroy
+  	redirect_to root_path
+  end
+
+  private
+
+  def meetup_params
+  	params.require(:meetup).permit(:title, :description)
+  end
 end
